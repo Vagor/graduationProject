dc0772cc953642f17d938aa7fe8d7e6302fa0451
1974331200 <1974331200@qq.com>
2017-04-25 13:29:48 +0800

    Revert "Revert "完成/pullQuestionsToPaper接口（将三种题型拉入题库）""
    This reverts commit c13deb6d04ae1cfc33bd8f34a90bbff12646cec4.

--- backEnd/handlers/paper/motherPaperHandler.js
+++ backEnd/handlers/paper/motherPaperHandler.js
@@ -52,10 +52,10 @@ module.exports = {
                 for (Index in data) {
                     choiceQList.push(data[Index].choiceQId)
                 }
-                res.send({choiceQList})
+                res.send({ choiceQList })
             })
     },
-     //通过paperId返回填空题信息
+    //通过paperId返回填空题信息
     getFQList: function (req, res) {
         var paperId = req.body.paperId
         PaperFQModel
@@ -70,10 +70,10 @@ module.exports = {
                 for (Index in data) {
                     fQList.push(data[Index].fQId)
                 }
-                res.send({fQList})
+                res.send({ fQList })
             })
     },
- //通过paperId返回简答题信息
+    //通过paperId返回简答题信息
     getSQList: function (req, res) {
         var paperId = req.body.paperId
         PaperSQModel
@@ -88,7 +88,109 @@ module.exports = {
                 for (Index in data) {
                     sQList.push(data[Index].sQId)
                 }
-                res.send({sQList})
+                res.send({ sQList })
             })
+    },
+    //将三种题型拉入组卷，拉入的信息包含各题分值和总分值
+    pullQuestions: function (req, res) {
+        var paperId = req.body.paperId
+        //更新总分
+        var conditions = { _id: paperId }
+        var update = {
+            $set: { totalScore: req.body.totalScore }
+        }
+        var options = {
+            upsert: true
+        }
+        MotherPaperModel.update(conditions, update, options, function (error) {
+            if (error) {
+                console.log(error)
+                res.json({ "success": 0 })
+            } else {
+                console.log('update ok!')
+                res.json({ "success": 1 })
+            }
+        })
+        //建立选择题与组卷关系表并存入分值
+        //var choiceQList = req.body.choiceQList
+        var choiceQList = [
+            {
+                choiceQId: "58c8f39ec7129e2b9cfd0868",
+                questionScore: 56
+            },
+            {
+                choiceQId: "58c8f39ec7129e2b9cfd0868",
+                questionScore: 78
+            }
+        ]
+        for (Index in choiceQList) {
+            var paperChoiceEntity = new PaperChoiceQModel({
+                paperId: paperId,
+                choiceQId: choiceQList[Index].choiceQId,
+                attr: { questionScore: choiceQList[Index].questionScore },
+            })
+            paperChoiceEntity.save(function (err, paperChoice) {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    console.log("create paper choice ralation ok") 
+                }
+            })
+        }
+      //建立填空题与组卷关系表并存入分值  
+      //var fQList= req.body.fQList
+        var fQList= [
+            {
+                fQId: "58c8f39ec7129e2b9cfd0868",
+                questionScore: 56
+            },
+            {
+                fQId: "58c8f39ec7129e2b9cfd0868",
+                questionScore: 78
+            }
+        ]
+        for (Index in fQList) {
+            var PaperFQEntity = new PaperFQModel({
+                paperId: paperId,
+                fQId: fQList[Index].fQId,
+                attr: { questionScore: fQList[Index].questionScore },
+            })
+            PaperFQEntity.save(function (err, PaperFQ) {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    console.log("create paper fillQuestion ralation ok") 
+                }
+            })
+        }
+        //建立简答题与组卷关系表并存入分值  
+      //var sQList= req.body.sQList
+        var sQList= [
+            {
+                sQId: "58c8f39ec7129e2b9cfd0868",
+                questionScore: 56
+            },
+            {
+                sQId: "58c8f39ec7129e2b9cfd0868",
+                questionScore: 78
+            }
+        ]
+        for (Index in sQList) {
+            var PapersQEntity = new PaperSQModel({
+                paperId: paperId,
+                sQId: sQList[Index].sQId,
+                attr: { questionScore: sQList[Index].questionScore },
+            })
+            PapersQEntity.save(function (err, PapersQ) {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    console.log("create paper SQuestion ralation ok") 
+                }
+            })
+        }
     },
 }
