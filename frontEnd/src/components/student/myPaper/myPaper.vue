de7b35f35b4e309e605996c846cf57eb008a2793
taolinwei <lwtao@Ctrip.com>
2017-05-06 09:56:43 +0800

    完成了学生端：查看自己的课程页面
    等待后端完成getPaperToCheckCount接口

--- frontEnd/src/components/student/myPaper/myPaper.vue
+++ frontEnd/src/components/student/myPaper/myPaper.vue
@@ -9,13 +9,64 @@
     <mt-cell title="微积分" value="" is-link class="left" :to="{name:'s_myPaperList'}">
       <mt-badge type="primary">1</mt-badge>
     </mt-cell>
-    <mt-cell title="电路理论" value="" is-link class="left" :to="{name:'nothing'}"></mt-cell>
-    <mt-cell title="数据结构" value="" is-link class="left" :to="{name:'nothing'}"></mt-cell>
+    <mt-cell :title="item.courseName" class="left" v-for="(item,index) in courseList" is-link>
+      <mt-badge type="primary" v-if="item.paperToCheckCount>0">{{item.paperToCheckCount}}</mt-badge>
+    </mt-cell>
   </div>
 </template>
 
 <script>
-  
+  export default {
+    name: "viewLesson",
+    data() {
+      return {
+        title: '我的课堂',
+        courseList: [{
+          courseName: "微积分",
+          lessonId: '123',
+          paperToCheckCount: 0,
+        }, {
+          courseName: "电路理论",
+          lessonId: '123',
+        }, {
+          courseName: "大学物理",
+          lessonId: '123',
+        }, {
+          courseName: "大学语文",
+          lessonId: '123',
+        }, {
+          courseName: "大学英语",
+          lessonId: '123',
+        }, {
+          courseName: "数据结构",
+          lessonId: '123',
+        }, ]
+      }
+    },
+    mounted: function() {
+      // 初始化lesson列表
+      this.$http.post('/getStudentLList', {
+        studentId: window._const.studentId,
+      }).then((res) => {
+        for (let key in res.data.lessonList) {
+          if (res.data.lessonList.hasOwnProperty(key)) {
+            this.courseList.push({
+              courseName: res.data.lessonList[key].courseName,
+              lessonId: res.data.lessonList[key]._id,
+            })
+            this.$http.post('/getPaperToCheckCount', {
+              lessonId: res.data.lessonList[key]._id,
+              studentId: window._const.studentId,
+            }).then((res_in) => {
+              this.courseList[key].paperToCheckCount = res_in.data.paperToCheckCount
+            })
+          }
+        }
+      })
+      // 初始化是否有待做试卷列表
+    },
+    methods: {},
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
